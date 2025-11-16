4cfecab83601055126866e93c6ac69f93b8a7430
karimbyd10 <zrika9324@gmail.com>
2025-11-16 17:09:27 +0100

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
+
+
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
-    
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
