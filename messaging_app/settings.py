e26f68bcfd1cb10394e58d084a7a435bd937e5c0
karimbyd10 <zrika9324@gmail.com>
2025-11-21 22:21:30 +0100

    settings.py

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -1,15 +1,27 @@
+# settings.py
+from datetime import timedelta
+
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    # Third-party apps
-    'rest_framework',
-
-    # Local apps
-    'chats',
+    # Django apps...
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "chats",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        # Optional: allow browser session login
+        # "rest_framework.authentication.SessionAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
 
