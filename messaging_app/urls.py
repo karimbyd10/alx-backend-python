42215a258c8a0fc437f677058bdf0ebbca976f51
karimbyd10 <zrika9324@gmail.com>
2025-11-21 22:23:47 +0100

    urls.py '

---
+++ messaging_app/urls.py
@@ -0,0 +1,19 @@
+# messaging_app/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+
+    # Chats API routes
+    path("api/", include("chats.urls")),
+
+    # JWT Auth endpoints
+    path("api/auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
+
