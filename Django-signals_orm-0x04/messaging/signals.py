dd18792b16bda8f97438822556b0c69ca1d5c045
karimbyd10 <zrika9324@gmail.com>
2025-11-27 22:59:23 +0100

    signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,13 +1,22 @@
-# messaging/signals.py
-from django.db.models.signals import post_save
+from django.db.models.signals import post_delete
+from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Message, Notification
 
-@receiver(post_save, sender=Message)
-def create_message_notification(sender, instance, created, **kwargs):
-    if created:
-        Notification.objects.create(
-            user=instance.receiver,
-            message=instance
-        )
+from .models import Message, Notification, MessageHistory
+
+
+@receiver(post_delete, sender=User)
+def cleanup_user_related_data(sender, instance, **kwargs):
+    """
+    Automatically deletes related records when a User is deleted.
+    Runs AFTER the user instance is removed from the database.
+    """
+    user = instance
+
+    Message.objects.filter(sender=user).delete()
+    Message.objects.filter(receiver=user).delete()
+
+    Notification.objects.filter(user=user).delete()
+
+    MessageHistory.objects.filter(user=user).delete()
 
