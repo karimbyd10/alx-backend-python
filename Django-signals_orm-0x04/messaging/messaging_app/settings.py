8a0a5a860e03145e03771a9b4e5c386957584a25
karimbyd10 <zrika9324@gmail.com>
2025-11-30 23:12:55 +0100

    settings.py

--- Django-signals_orm-0x04/messaging/messaging_app/settings.py
+++ Django-signals_orm-0x04/messaging/messaging_app/settings.py
@@ -1,7 +1,9 @@
+# messaging_app/messaging_app/settings.py
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
         'LOCATION': 'unique-snowflake',
     }
 }
-I
+
