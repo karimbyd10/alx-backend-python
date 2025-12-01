bd198725e5d06ce4fefd7335df5a2714070a91c9
karimbyd10 <zrika9324@gmail.com>
2025-12-01 21:47:26 +0100

    settings.py

---
+++ Django-Middleware-0x03/settings.py
@@ -0,0 +1,13 @@
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # Your custom middleware
+    'messagingapp.middleware.RequestLoggingMiddleware',
+]
+
