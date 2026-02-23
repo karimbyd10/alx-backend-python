a8e43334a17da8e14f2c47ad8af59c32a81b0cb1
karimbyd10 <zrika9324@gmail.com>
2026-02-23 02:10:42 +0000

    urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),  # <-- This registers the chats API under /api
+    path('api-auth/', include('rest_framework.urls')),  # DRF login/logout for browsable API
 ]
