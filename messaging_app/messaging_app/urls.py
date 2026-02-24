c570030a7883409d8752ff503e3a4eda322df3c2
karimbyd10 <zrika9324@gmail.com>
2026-02-24 15:42:08 +0000

    urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -16,9 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),  # <-- This registers the chats API under /api
     path('api-auth/', include('rest_framework.urls')),  # DRF login/logout for browsable API
+     # JWT endpoints
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # chats app
+    path('api/', include('messaging_app.chats.urls')),
 ]
